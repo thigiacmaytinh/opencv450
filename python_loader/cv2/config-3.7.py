5c78b3130294f1c5833b7b52fcf909d554243848
vohungvi <vohungvi27@gmail.com>
2023-03-27 10:01:30 +0700

    + import

---
+++ python_loader/cv2/config-3.7.py
@@ -0,0 +1,3 @@
+PYTHON_EXTENSIONS_PATHS = [
+    'D:/LIB/opencv/opencv450/build/lib/python3/Release'
+] + PYTHON_EXTENSIONS_PATHS
---
+++ python_loader/cv2/config.py
@@ -0,0 +1,5 @@
+import os
+
+BINARIES_PATHS = [
+    'D:/LIB/opencv/opencv450/build/bin/Release'
+] + BINARIES_PATHS
