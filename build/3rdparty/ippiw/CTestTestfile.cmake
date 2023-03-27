16abe35edfa63c761598a435fb3cb8ee70f831cb
vohungvi <vohungvi27@gmail.com>
2023-03-27 09:57:54 +0700

    + import

---
+++ build/3rdparty/ippiw/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/LIB/opencv/opencv450/build/3rdparty/ippicv/ippicv_win/iw
+# Build directory: D:/LIB/opencv/opencv450/build/3rdparty/ippiw
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
